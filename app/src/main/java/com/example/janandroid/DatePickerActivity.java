826ce88934bb6c5ec36b2f826dd4d6a273bf9408
Deepak <deepakcse31@gmail.com>
2026-01-12 19:46:17 +0530

    BottomSheet/TImePicker/DatePIcker

---
+++ app/src/main/java/com/example/janandroid/DatePickerActivity.java
@@ -0,0 +1,58 @@
+package com.example.janandroid;
+
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Calendar;
+
+public class DatePickerActivity extends AppCompatActivity {
+    Button btnPopenDatePicker;
+    TextView tvdate;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_date_picker);
+        tvdate=findViewById(R.id.tvdate);
+
+        btnPopenDatePicker=findViewById(R.id.btnDatePicker);
+        btnPopenDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar=Calendar.getInstance();
+                int day=calendar.get(Calendar.DAY_OF_MONTH);
+                int month=calendar.get(Calendar.MONTH);
+                int year=calendar.get(Calendar.YEAR);
+
+                DatePickerDialog datePickerDialog=new DatePickerDialog(DatePickerActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                                String dae=i+"/"+(i1+1)+"/"+i2;
+                                tvdate.setText(dae);
+                            }
+                        },year,month,day);
+                datePickerDialog.show();
+            }
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
